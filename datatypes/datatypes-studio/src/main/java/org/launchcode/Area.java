83043508c0caa45f6766806c16665df86714edf6
Tim-E <caffeinated.ink@gmail.com>
2024-04-17 22:53:25 -0400

    data types studio complete

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,2 +1,12 @@
-package org.launchcode;public class Area {
+package org.launchcode;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a radius:");
+        Double radius = input.nextDouble();
+
+        System.out.println("The area of a circle with a radius of " + radius + " is " + Math.PI * radius * radius);
+    }
 }
